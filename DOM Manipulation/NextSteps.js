f3cc50708a5974f4d602a73fc5f6f80d4f295df7
Supriya Adep <supriya.adep@fractalanalytics.com>
2020-02-13 23:36:33 +0530

    first-commit

---
+++ DOM Manipulation/NextSteps.js
@@ -0,0 +1,22 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he wishes. 
+  Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. 
+  After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added 
+  to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+*/
+/*
+NEXT STEPS
+
+Change the game to follow these rules
+ - A player looses his ENTIRE score when he rolls two 6 in a row. After that it is the
+ next player's turn. (Hint: Always store the prevoius dice roll in a separate variable)
+ - Add a input field to the HTML, where the players can set the winning goal
+ - Add one more dice, so that there are two dices now. The player loses his current score when
+ one of them is 1
+*/
